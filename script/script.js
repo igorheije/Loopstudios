2abb69a6beb549800a057493b0988ea2ece15635
igorheije <heije.tomori2014@outlook.com>
2021-01-11 10:10:41 -0300

    mobile4

--- script/script.js
+++ script/script.js
@@ -1,26 +1,23 @@
-const anima = window.innerHeight
-const scrollY = window.scrollY
 const img = document.querySelector('.main-img')
 const info = document.querySelector('.main-info')
 const card = document.querySelectorAll('.card')
+
+
 const getWindowBottom = () => {
     const teste = window.scrollY + window.innerHeight
     if(teste>800){
         img.classList.add('esquerda')
         info.classList.add('direita')
     }
-    console.log(card);
-    
     card.forEach((card, i)=>{
-        if(teste>1200+i*100){
+        if(teste>1300+i*50){
             if(i%2===0){
                 card.classList.add('esquerda')
             }else{
                 card.classList.add('direita')
             }
-    }
+        }
     })
-    
 }
 
 document.addEventListener('scroll',getWindowBottom)
